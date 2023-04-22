563f0b7c7c64bc396352e30ff47f0a9b713f61dc
vrym2 <vardhan609@gmail.com>
2023-04-22 22:08:40 +0000

    filter json data planet

---
+++ src/data/planet_download.py
@@ -0,0 +1,25 @@
+import os
+import json
+import asyncio
+import logging
+import logging.config
+from typing import List, Dict
+
+# Loading the config file
+logging.config.fileConfig('logger.ini')
+
+
+async def data_download(
+        search_json: dict = None):
+    """Function to download Planet data
+    
+    Args:
+        Path to JSON data file with search results
+    """
+    # Reading the JSON file
+    with open(search_json, 'r') as scenes:
+        json_scenes = json.load(scenes)
+    
+    # Filtering scenes
+    clear_confidence = filter(lambda a: a['properties']['clear_confidence_percent'] > 50, json_scenes)
+    return list(clear_confidence)
